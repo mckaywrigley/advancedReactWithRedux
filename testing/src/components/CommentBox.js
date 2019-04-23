76e376e3916f84637d87a290a0147ad8773632d9
Mckay <mckaywrigley@gmail.com>
2019-04-23 14:00:41 -0700

    creates text area and button tests for CommentBox

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
 
 class CommentBox extends Component {
+  state = { comment: "" };
+
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      comment: ""
+    });
+  };
+
   render() {
     return (
-      <div>
-        <div>Comment Box</div>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea
+          name="comment"
+          value={this.state.comment}
+          onChange={this.handleInput}
+        />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
     );
   }
 }
